93bbca31856323f6fafb457dddc48b1360a51913
anusunny339 <61958678+anusunny339@users.noreply.github.com>
2022-05-10 16:24:52 +0530

    intial commit

---
+++ CO4/package/Triangle.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package graphics;
+
+/**
+ *
+ * @author sjcet
+ */
+public class Triangle {
+     public void area(double b,double h){
+        System.out.println("Area of triangle:"+((b*h)/2));
+    }
+    
+}
