3a7bbb16ed878cb94c001ae2326243bc22411a16
anusunny339 <61958678+anusunny339@users.noreply.github.com>
2022-05-10 16:24:26 +0530

    intial commit

---
+++ CO4/package/Rectangle.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package graphics;
+
+/**
+ *
+ * @author sjcet
+ */
+
+
+public class Rectangle {
+    
+    public void area(double l,double b){
+        System.out.println("Area of rectangle:"+(l*b));
+    }
+    
+}
