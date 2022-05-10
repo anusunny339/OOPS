c74573d336b9246282386d4f93338f3f6720b8ee
anusunny339 <61958678+anusunny339@users.noreply.github.com>
2022-05-10 16:25:37 +0530

    first commit

---
+++ CO4/package/Circle.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package graphics;
+
+/**
+ *
+ * @author sjcet
+ */
+public class Circle {
+     public void area(double r){
+        System.out.println("Area of circle: "+Math.PI*(r*r));
+    }
+    
+}
