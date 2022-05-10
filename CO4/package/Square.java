e3ed0e8d9df4dcf4775e3afc930b1a05288e90a5
anusunny339 <61958678+anusunny339@users.noreply.github.com>
2022-05-10 16:25:14 +0530

    intial commit

---
+++ CO4/package/Square.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package graphics;
+
+/**
+ *
+ * @author sjcet
+ */
+public class Square {
+     public void area(double a){
+        System.out.println("Area of square:"+(4*a));
+    }
+    
+}
